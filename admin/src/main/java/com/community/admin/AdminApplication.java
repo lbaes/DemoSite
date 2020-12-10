2a511c090a735e3714585a5dd3249cd59209dd9f
Lucas Baes <lucasfranchinibaes@gmail.com>
2020-12-09 21:56:23 -0300

    reverted to .jar

--- admin/src/main/java/com/community/admin/AdminApplication.java
+++ admin/src/main/java/com/community/admin/AdminApplication.java
@@ -5,21 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class AdminApplication extends SpringBootServletInitializer{
+public class AdminApplication {
 
     @Configuration
     @EnableBroadleafAdminAutoConfiguration
     public static class BroadleafFrameworkConfiguration {}
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(AdminApplication.class);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
--- api/src/main/java/com/community/api/ApiApplication.java
+++ api/src/main/java/com/community/api/ApiApplication.java
@@ -5,24 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * @author Elbert Bautista (elbertbautista)
  */
 @SpringBootApplication
 @EnableAutoConfiguration
-public class ApiApplication extends SpringBootServletInitializer{
+public class ApiApplication {
 
     @Configuration
     @EnableBroadleafSiteRootAutoConfiguration
     public static class BroadleafFrameworkConfiguration {}
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ApiApplication.class);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
